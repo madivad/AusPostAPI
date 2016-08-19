ac7553b2c3d66f6e9041cf08d1c437c3a3cec18d
madivad <madivad@dav3.net>
2016-08-19 15:07:39 +1000

    Create kile.php
    This is the api-key location

---
+++ kfile.php
@@ -0,0 +1,6 @@
+<?php
+/* this is the key file, store your API key in this file
+   you should get your key from: 
+   https://developers.auspost.com.au/
+   */
+  	$akey = 'insert-your-api-key-here';
